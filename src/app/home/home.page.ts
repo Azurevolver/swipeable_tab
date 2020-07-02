0e190f3d89db7daa54221b8856399636caf48cbc
yuanchingchen <yuanching.chen.appledev@gmail.com>
2020-07-01 21:02:28 -0500

    [Update] sync the home page back button

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,18 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, AfterViewInit } from '@angular/core';
 import { NewsPage } from '../news/news.page';
 import { AccountPage } from '../account/account.page';
 import { AboutPage } from '../about/about.page';
+import { IonNav } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements AfterViewInit{
   newsPage = NewsPage;
   accountPage = AccountPage;
   aboutPage = AboutPage;
   
+  canGoBack = false;
+
+  @ViewChild('newsNav', {static: false}) newsNav:IonNav;
+
   constructor() {}
+
+  goBack() {
+    this.newsNav.pop();
+  }
+
+  ngAfterViewInit() {
+    this.newsNav.ionNavDidChange.subscribe(async ev => {
+      console.log('[debug] home page listner - event: ', ev);
+      this.canGoBack = await this.newsNav.canGoBack();
+    })
+  }
 
 }
